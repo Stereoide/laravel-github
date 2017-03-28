9c6eedc72727eaafa439d08cab9edc982ca226d2
Stereoide <Stereoide@gmail.com>
2017-03-28 23:32:06 +0200

    Added public events route

--- src/routes/routes.php
+++ src/routes/routes.php
@@ -1,5 +1,6 @@
 <?php
 
 Route::get('github/getUserRepos/{username}', '\Stereoide\Github\GithubController@getUserRepos');
+Route::get('github/events/{page}', '\Stereoide\Github\GithubController@events');
 
 Route::get('github/{cmd}', '\Stereoide\Github\GithubController@cmd');
