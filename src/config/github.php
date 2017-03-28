07ddba94a2af3f3c6ce30a4b0986a4a3813e7ef6
Stereoide <Stereoide@gmail.com>
2017-03-28 22:41:11 +0200

    Added oAuthToken explaination to config file

--- src/config/github.php
+++ src/config/github.php
@@ -1,5 +1,15 @@
 <?php
 
 return [
+    /*
+    |--------------------------------------------------------------------------
+    | oAuth Token
+    |--------------------------------------------------------------------------
+    |
+    | Some API calls need to be authenticated via an oAuth token.
+    | Get yours at https://github.com/settings/tokens
+    |
+    */
+
     'oAuthToken' => '',
 ];
