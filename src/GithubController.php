dd00c2208793c4ca627565655bfd2693e9305737
Stereoide <Stereoide@gmail.com>
2017-03-30 23:42:59 +0200

    Added method to fetch repository issues events

--- src/GithubController.php
+++ src/GithubController.php
@@ -219,4 +219,28 @@ class GithubController extends \App\Http\Controllers\Controller
 
         return $events;
     }
+
+    /**
+     * @param string $owner
+     * @param string $repository
+     * @param int $paginationOffset
+     * @return mixed
+     * @see https://developer.github.com/v3/activity/events/#list-issue-events-for-a-repository
+     */
+    public function getRepositoryIssuesEvents($owner, $repository, $paginationOffset = 1)
+    {
+        /* Fetch repository events */
+
+        list($statusCode, $headers, $body) = GithubController::request('repos/' . $owner . '/' . $repository . '/issues/events', 'GET', [], $paginationOffset);
+
+        $events = collect($body);
+
+        /* Determine pagination data */
+
+        $pagination = GithubController::getPaginationFromResponseHeaders($headers);
+
+        /* Return public events */
+
+        return $events;
+    }
 }
--- src/routes/routes.php
+++ src/routes/routes.php
@@ -6,6 +6,7 @@ Route::get('github/getUserRepos/{username}', '\Stereoide\Github\GithubController
 
 Route::get('github/events', '\Stereoide\Github\GithubController@getEvents');
 Route::get('github/repositoryEvents/{owner}/{repository}', '\Stereoide\Github\GithubController@getRepositoryEvents');
+Route::get('github/repositoryIssuesEvents/{owner}/{repository}', '\Stereoide\Github\GithubController@getRepositoryIssuesEvents');
 
 /* Catch-all */
 
