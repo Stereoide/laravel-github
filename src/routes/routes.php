fa8b78e628761af692c00a81df8a0067a9c90d24
Stereoide <Stereoide@gmail.com>
2017-03-30 23:28:31 +0200

    Introduced sections to package route file

--- src/routes/routes.php
+++ src/routes/routes.php
@@ -1,6 +1,11 @@
 <?php
 
 Route::get('github/getUserRepos/{username}', '\Stereoide\Github\GithubController@getUserRepos');
-Route::get('github/events/{page}', '\Stereoide\Github\GithubController@events');
+
+/* Events */
+
+Route::get('github/events', '\Stereoide\Github\GithubController@getEvents');
+
+/* Catch-all */
 
 Route::get('github/{cmd}', '\Stereoide\Github\GithubController@cmd');
